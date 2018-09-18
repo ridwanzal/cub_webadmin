8fdbfcba851b14f39e90c79335f51b83386f30df
AwalMuhibHalim <awalmuhibhalim@gmail.com>
2018-09-18 11:34:48 +0700

    connection

---
+++ es_webadmin/model/connection.php
@@ -0,0 +1,16 @@
+<?php
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASSWORD', '');
+define('DB_DATABASE', 'cloudinovasi');
+
+// connect to dbms
+$conn = mysqli_connect(DB_HOST, DB_USER, DB_PASSWORD, DB_DATABASE);
+if ($conn === false) {
+    echo '<alert>not connected</alert>';
+}
+//else{
+//    echo '<alert>connected</alert>';
+//}
+?>
